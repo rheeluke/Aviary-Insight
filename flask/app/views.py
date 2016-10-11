657b55961a3a36b9467c64e2512f313296994095
Luke Young Rhee <rheeluke@gmail.com>
2016-10-10 20:44:31 -0700

    refined flask app

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,12 +1,33 @@
+from app import app
+
 from flask import Flask, render_template, jsonify, url_for
 from pydruid.client import PyDruid
 from pydruid.utils import aggregators, filters
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/data/")
+@app.route("/data/<myfilter>")
+def data(myfilter=None):
+    top = computeQuery(myfilter)
+    children = []
+    for hashmap in top:
+        children.append(
+            {'name': hashmap['hashtags'],
+             'children': [{'name': 'loading...'}]})
+    my_data = {'name': 'top', 'children': children}
+    return jsonify(my_data)
 
 query = PyDruid(
     'http://ec2-54-71-20-46.us-west-2.compute.amazonaws.com:8082', 'druid/v2')
 
 
-def computeQuery(myfilter=None):
+def computeQuery(myfilter):
 
     if myfilter and 'badstr' in myfilter:
 
@@ -29,7 +50,6 @@ def computeQuery(myfilter=None):
             threshold=5+len(filterList),
             aggregations={'count': aggregators.count('count')},
             filter=queryFilter
-            # filter=filters.Dimension('hashtags') == filterList[0]
         )
         return top[0]['result'][len(filterList):]
     else:
@@ -43,36 +63,3 @@ def computeQuery(myfilter=None):
             aggregations={'count': aggregators.count('count')}
         )
         return top[0]['result']
-
-app = Flask(__name__)
-
-
-@app.route("/data/")
-@app.route("/data/<myfilter>")
-def data(myfilter=None):
-    top = computeQuery(myfilter=myfilter)
-    children = []
-    for hashmap in top:
-        children.append(
-            {'name': hashmap['hashtags'],
-             'children': [{'name': 'loading...'}]})
-    my_data = {'name': 'top', 'children': children}
-    return jsonify(my_data)
-
-
-@app.route("/root/")
-@app.route("/root/<rootFilter>")
-def root(rootFilter=None):
-    top = computeQuery(myfilter=rootFilter)
-    children = []
-    for hashmap in top:
-        children.append(
-            {'name': hashmap['hashtags'],
-             'children': [{'name': 'loading...'}]})
-    my_data = {'name': 'top', 'children': children}
-    return jsonify(my_data)
-
-
-@app.route('/')
-def hello_world():
-    return render_template('hello.html', rootFilter="")
---
+++ flask/bin/installFlask.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+peg scp to-rem lrh 4 ../app /home/ubuntu
+peg scp to-rem lrh 4 ../run.py /home/ubuntu
